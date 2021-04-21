9fc56caf24908ffb9c01cb703c3a13e5ab7de34a
jHapy Lead Dev <jhapy@jhapy.org>
2021-04-21 09:17:53 +0000

    Enhancement

--- src/main/java/org/jhapy/resource/endpoint/ResourceServiceEndpoint.java
+++ src/main/java/org/jhapy/resource/endpoint/ResourceServiceEndpoint.java
@@ -27,7 +27,6 @@ import org.jhapy.dto.serviceQuery.generic.SaveQuery;
 import org.jhapy.resource.domain.StoredFile;
 import org.jhapy.resource.service.ResourceService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
